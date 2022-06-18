f61ae193acb0845a221b12341e988deb0375f368
Christina R <xsnachannell@gmail.com>
2022-06-18 00:59:53 -0700

    updates script so that nav burger works when window is mobile sized

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,14 @@
-const portfolioProjects = {
-    projectHighlight: ["Here's a group Project I've just completed!"]
-}
+//code taken from Bulma's example code on nav burger here: https://bulma.io/documentation/components/navbar/
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+        });
+    });
+});
